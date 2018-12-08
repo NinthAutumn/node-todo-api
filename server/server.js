6b1d87df0c30b0764575ecb79982e80b8dd2498d
Arif Iwamoto <arif.iwamoto@gmail.com>
2018-12-08 15:16:08 +0700

    hashing password

--- server/server.js
+++ server/server.js
@@ -72,3 +72,14 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send()
   })
 })
+
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
+
+  user.save().then((user) => {
+    res.send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+})
